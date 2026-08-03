d43e2d4d3b82e0c2946e8a5f7448b174b855bccd
Ananya_Roy478 <in.ananyaroy@gmail.com>
2026-08-03 19:57:01 +0530

    Delete unwanted files

--- src/app.js
+++ src/app.js
@@ -1,17 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-
-// const routes = require("./routes/insight.routes");
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/api/insights", routes);
-
-// module.exports = app;
-
 const express = require("express");
 const cors = require("cors");
 const errorMiddleware = require("./middlewares/errorMiddleware");
